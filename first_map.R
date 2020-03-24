be340c98b14c4645ecf4c04c9131d6043cb4d410
Emma Ladouceur <emma.ladouceur@idiv.de>
2020-03-24 10:32:10 +0100

    wrangle and emmas map

--- first_map.R
+++ first_map.R
@@ -1,5 +1,4 @@
 
-install.packages("googlesheets4")
 
 library("googlesheets4")
 library(tidyverse)
@@ -11,10 +10,37 @@ library(viridis)
 library(hrbrthemes)
 library(mapdata)
 library(ggrepel)
-
+library(sp) # For converting to decimal degrees
 
 # Read our google sheet!
-sbm<-read_sheet("https://https://docs.google.com/spreadsheets/d/10H1CWb5cc2FNEzTjxROdZuT2F6DwXCa-Ng3_DAsZ2K4/edit#gid=0")
+sb<-read_sheet("https://https://docs.google.com/spreadsheets/d/10H1CWb5cc2FNEzTjxROdZuT2F6DwXCa-Ng3_DAsZ2K4/edit#gid=0")
+
+# Ali's Lat Long wrangling code
+sb<-sb[!is.na(sb$Lat_Deg) & !is.na(sb$Lon_Deg),] # remove rows that don't have both lat and long at degree resolution
+
+# First have to split the dataset into those with decimals and those without
+sb.dec<-sb[grep("\\.", sb$Lat_Deg),]
+sb<-sb[!grepl("\\.", sb$Lat_Deg),]
+
+# Then account for mistakes in coordinates - need to work out properly later, but do this for now
+sb<-sb[!sb$Lat_Deg>90,] # remove rows with impossible latitudes
+sb$Lat_Min[sb$Lat_Min>59]<-59; sb$Lon_Min[sb$Lon_Min>179]<-179 # Some places have minutes (and some seconds) above 60, which I think is impossible. Need to sort these out better eventually but the conversion seems to work anyway.
+
+# add zeroes for minutes/seconds where they are blank
+sb$Lon_Min[is.na(sb$Lon_Min)]<-0; sb$Lat_Min[is.na(sb$Lat_Min)]<-0  
+sb$Lat_Sec[is.na(sb$Lat_Sec)]<-0; sb$Lon_Sec[is.na(sb$Lon_Sec)]<-0 
+
+# for conversion - first paste together the coordinates with d, m, s as separators (needed later)
+sb$Lat<-paste0(sb$Lat_Deg,"d",sb$Lat_Min,"m",round(as.numeric(sb$Lat_Sec)),"s",sb$Lat_NS) 
+sb$Lon<-paste0(sb$Lon_Deg,"d",sb$Lon_Min,"m",round(as.numeric(sb$Lon_Sec)),"s",sb$Lon_EW)
+
+# then char2dms converts the coordinates to decimals, using the separators we just added. Overwrite original column
+sb$Lat_Deg<-as.numeric(char2dms(sb$Lat,"d","m","s"))
+sb$Lon_Deg<-as.numeric(char2dms(sb$Lon,"d","m","s"))
+
+sb<-sb[,1:(ncol(sb)-2)] # removing the new pre-conversion columns so that data frames line up again 
+
+sb<-rbind(sb,sb.dec) # bind back together
 
 # This code copied from 'Data to Vis'  here:
 # https://www.data-to-viz.com/graph/bubblemap.html
@@ -29,12 +55,10 @@ ggplot(data = world) +
 # Get the world polygon
 world <- map_data("world")
 
-colnames(sbm)
+colnames(sb)
 
-# need to reformat lat and long data
-# but for a 'first crack'....
 
-gsbm <- sbm %>%
+gsbm <- sb %>%
   #group_by(latitude, longitude, site_code, `Length of study`, continent) %>%
   ggplot() +
   geom_polygon(data = world, aes(x=long, y = lat, group = group), fill="grey", alpha=0.7) +
@@ -53,11 +77,11 @@ gsbm <- sbm %>%
   theme(
     panel.spacing=unit(c(0,0,0,0), "null"),
     plot.margin=grid::unit(c(1,1,1,1), "cm"),
-    legend.position=c(0.17,0.001),
+    legend.position=c(0.20,0.001),
     legend.direction="horizontal"
   ) +
-  ggplot2::annotate("text", x = -185, y = -34, hjust = 0, size = 7, label = paste("Soil Seedbanks of the World"), color = "Black") +
-  ggplot2::annotate("text", x = -181, y = -44, hjust = 0, size = 4, label = paste("Experimental Locations"), color = "black", alpha = 0.5) +
+  ggplot2::annotate("text", x = -190, y = -34, hjust = 0, size = 7, label = paste("Soil Seedbanks of the World"), color = "Black") +
+  ggplot2::annotate("text", x = -190, y = -44, hjust = 0, size = 4, label = paste("Experimental Locations"), color = "black", alpha = 0.5) +
   xlim(-180,180) +
   ylim(-60,80) +
   scale_x_continuous(expand = c(0.006, 0.006)) +
