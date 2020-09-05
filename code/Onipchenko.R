3e4cc752ba608150a1ed6ce03458305acf51f3ab
Emma Ladouceur <emma.ladouceur@idiv.de>
2020-09-05 09:56:35 +0200

    russia data

--- code/Onipchenko.R
+++ code/Onipchenko.R
@@ -24,26 +24,43 @@ siteref <- refs %>% left_join(sites)
 
 head(siteref)
 
-# species and density sheet seem to be the same
-# use the species sheet
+# species and density sheet are the same
+# use the density sheet
+head(density)
+View(density)
 
-
-head(species)
-
-species_summary <- species %>% filter(Seedbank.present == 1) %>% # only seedbank records, not vegetation records
+species_summary <- density %>% filter(Seedbank.present == 1) %>% # only seedbank records, not vegetation records
   group_by(Reference) %>% # ignore trial and layer , and only summarise species by study (reference)
   summarise(
-    Species_Richness = n_distinct(Species) ) %>%
-  rename(`Ref.No` = Reference ) # rename reference column to ref,no to match siteref sheet
-
+    Species_Richness = n_distinct(Species), # count the species
+    Density = sum(Density) ) %>% # sum the densities
+  rename(`Ref.No` = Reference ) # rename reference column to ref.no to match 'siteref' sheet
 
 
 View(species_summary)
 
 
- sum_dat <- siteref %>% left_join(species_summary) # join site and reference info with species richness info
+trial <- density %>% select(Reference, Trial) %>% #select columns of trial number and study
+  distinct() %>% # get distinct values
+  group_by(Reference) %>% # group by reference
+  filter(Trial == max(Trial)) # take the max value 
+
+trial$Reference <- as.factor(as.character(trial$Reference))
+
+ sum_dat <- siteref %>% left_join(species_summary) %>% # join site and reference info with species richness info
+   select(-Trial) %>%
+   left_join(trial) %>%
+   mutate(  Volume.sampled.cm = (Area.sampled * Total.depth),
+            Volume.sampled.mm = (Volume.sampled.cm * 1000 ) ,
+            Total.Volume.mm = (Volume.sampled.mm * Trial)) # assuming the volume is cm3 X 1000 for mm3
+ 
 
  
- View(sum_dat)
+head(sum_dat)
+summary(sum_dat) # only 1 NA where species richness and density was not reported 
+
+View(sum_dat)
+
+
 
 
