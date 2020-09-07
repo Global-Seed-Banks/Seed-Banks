b0ef163720307bd83922cec019b1698adf8e89b8
Emma Ladouceur <emma.ladouceur@idiv.de>
2020-09-07 12:09:32 +0200

    Onipchenko processing code

--- code/Onipchenko.R
+++ code/Onipchenko.R
@@ -8,7 +8,7 @@ library(tidyverse)
 library(openxlsx)
 
 
-
+# Emma's paths
 density <- read.xlsx("~/Dropbox/Projects/SeedbankMap/data/Onipchenko/RDB.xlsx", sheet = 1)
 species <- read.xlsx("~/Dropbox/Projects/SeedbankMap/data/Onipchenko/RDB.xlsx", sheet = 2)
 sites <- read.xlsx("~/Dropbox/Projects/SeedbankMap/data/Onipchenko/RDB.xlsx", sheet = 3)
@@ -29,37 +29,59 @@ head(siteref)
 head(density)
 View(density)
 
+species_density <- density %>% filter(Seedbank.present == 1) %>% # only seedbank records, not vegetation records
+  group_by(Reference, Trial) %>% # group by study and by trial
+  summarise( 
+    Density_per_trial = sum(Density) ) %>% # sum the density of each species per trial
+  ungroup() %>% # ungroup data
+  group_by(Reference) %>% # regroup only by reference/study
+  summarise( 
+    Avg_density_across_trials = mean(Density_per_trial) ) %>% # average the density across trials
+  rename(`Ref.No` = Reference ) # rename reference column to ref.no to match 'siteref' sheet
+
+
+View(species_density)
+
 species_summary <- density %>% filter(Seedbank.present == 1) %>% # only seedbank records, not vegetation records
   group_by(Reference) %>% # ignore trial and layer , and only summarise species by study (reference)
   summarise(
-    Species_Richness = n_distinct(Species), # count the species
-    Density = sum(Density) ) %>% # sum the densities
-  rename(`Ref.No` = Reference ) # rename reference column to ref.no to match 'siteref' sheet
+    Species_Richness = n_distinct(Species) ) %>% # count the total species per study
+  rename(`Ref.No` = Reference ) %>% # rename reference column to ref.no to match 'siteref' sheet 
+left_join(species_density)
 
 
 View(species_summary)
 
 
-trial <- density %>% select(Reference, Trial) %>% #select columns of trial number and study
+
+trial_dat <- density %>% select(Reference, Trial) %>% #select columns of trial number and study
   distinct() %>% # get distinct values
   group_by(Reference) %>% # group by reference
-  filter(Trial == max(Trial)) # take the max value 
+  filter(Trial == max(Trial)) %>% # take the max value 
+  rename(`Ref.No` = Reference ) # rename reference column to ref.no to match 'siteref' sheet 
 
-trial$Reference <- as.factor(as.character(trial$Reference))
+trial_dat$`Ref.No` <- as.factor(as.character(trial_dat$`Ref.No`))
+siteref$`Ref.No` <- as.factor(as.character(siteref$`Ref.No`))
+species_summary$`Ref.No` <- as.factor(as.character(species_summary$`Ref.No`))
 
- sum_dat <- siteref %>% left_join(species_summary) %>% # join site and reference info with species richness info
+
+View(trial_dat)
+ 
+sum_dat <- siteref %>% left_join(species_summary) %>% # join site and reference info with species richness info
    select(-Trial) %>%
-   left_join(trial) %>%
+   left_join(trial_dat) %>%
    mutate(  Volume.sampled.cm = (Area.sampled * Total.depth),
-            Volume.sampled.mm = (Volume.sampled.cm * 1000 ) ,
-            Total.Volume.mm = (Volume.sampled.mm * Trial)) # assuming the volume is cm3 X 1000 for mm3
+            Volume.sampled.mm = (Volume.sampled.cm * 1000 ) , # assuming the volume is cm3 X 1000 for mm3
+            Total.Volume.mm = (Volume.sampled.mm * Trial)) # i think the size of sample is per trial... or per sample
  
 
  
 head(sum_dat)
 summary(sum_dat) # only 1 NA where species richness and density was not reported 
+colnames(sum_dat)
 
-View(sum_dat)
+# Emma's path
+write.csv(sum_dat, "~/Dropbox/Projects/SeedbankMap/data/Onipchenko/processed_data.csv")
 
 
 
