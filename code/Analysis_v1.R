e7ccd2cebc9865104d0e7cbe09cf52ea533f6ab5
AGAuffret <alistair.auffret@slu.se>
2021-10-27 15:55:25 +0200

    new analysis script

---
+++ code/Analysis_v1.R
@@ -0,0 +1,17 @@
+#####################################
+## GLOBAL SEED BANKS #############
+## Analysis... I guess ##
+
+sb<-read.csv("gsb_slim.csv", stringsAsFactors = FALSE)
+
+length(unique(sb$studyID))
+nrow(sb)
+sum(sb$Total_Number_Samples, na.rm=TRUE)
+length(unique(sb$Country))
+
+sb[is.na(sb$Total_Number_Samples),]
+
+
+head(sb)
+
+plot(Total_Species~log(Sample_Area_mm2), data=sb)
