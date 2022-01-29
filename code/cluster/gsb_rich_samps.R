87d03ba5a64dab4ed6f0287174b5aed157da021c
Emma Ladouceur <emmala@gmail.com>
2022-01-29 11:11:47 +0100

    adapt delta is causing probs, skip for now

--- code/cluster/gsb_rich_samps.R
+++ code/cluster/gsb_rich_samps.R
@@ -9,8 +9,9 @@ sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.st
 
 rich.samps <- brm(Total_Species2 ~ log_Total_Number_Samples * Biome_WWF_Zone + (log_Total_Number_Samples * Biome_WWF_Zone  | Habitat_Broad/studyID/samp.loc ), 
                 family = poisson(), data = sb, cores = 4, chains = 4,
-                iterations = 10000, warmup = 1000,
-                control = list(adapt_delta = 0.99) )
+                iter = 10000, warmup = 1000
+               # control = list(adapt_delta = 0.99) 
+                )
 
 
 save(rich.samps,
