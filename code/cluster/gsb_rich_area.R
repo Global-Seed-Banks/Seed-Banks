27f3b01432e6922b2777bc7dc3cc1da479144536
Emma Ladouceur <emmala@gmail.com>
2022-02-07 08:56:12 +0100

    half way through soem stuff

--- code/cluster/gsb_rich_area.R
+++ code/cluster/gsb_rich_area.R
@@ -7,13 +7,14 @@ path <- '/gpfs1/data/idiv_chase/emmala/Seed-Bank-Map'
 sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
 
 
-# rich.area <- brm(Total_Species2 ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (log_Total_Sample_Area_mm2 * Biome_WWF_Zone  | Habitat_Broad/studyID/rowID ),
-#                 family = poisson(), data = sb, cores = 4, chains = 4,
-#                 )
+rich.area <- brm(Total_Species2 ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (log_Total_Sample_Area_mm2 * Biome_WWF_Zone  | Habitat_Broad/studyID/rowID ),
+                family = poisson(), data = sb, cores = 4, chains = 4,
+                iter = 5000, warmup = 1000
+                )
 
-rich.area <- brm(log_Total_Species2 ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (log_Total_Sample_Area_mm2 * Biome_WWF_Zone  | Habitat_Broad/studyID/rowID ),
-                 family = student(), data = sb, cores = 4, chains = 4,
-)
+# rich.area <- brm(log_Total_Species2 ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (log_Total_Sample_Area_mm2 * Biome_WWF_Zone  | Habitat_Broad/studyID/rowID ),
+#                  family = student(), data = sb, cores = 4, chains = 4,
+# )
 
 
 save(rich.area,
