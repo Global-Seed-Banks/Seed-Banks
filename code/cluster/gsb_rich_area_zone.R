da57672e7d27d4197821f4008ee6ed41857f133a
Emma Ladouceur <emmala@gmail.com>
2022-05-03 18:57:00 +0200

    rich2

--- code/cluster/gsb_rich_area_zone.R
+++ code/cluster/gsb_rich_area_zone.R
@@ -19,10 +19,13 @@ sb_dat <- sb %>% filter(!is.na(Total_Species2),
           Method = as.factor(Method))
 # %>% filter(!Habitat_Broad == "Arable")
 
-rich.p_zones <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (log_Total_Sample_Area_mm2 * Biome_WWF_Zone  | Habitat_Broad ),
-                family = poisson(), data = sb_dat, cores = 4, chains = 4,
-                )
+# rich.p_zones <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (log_Total_Sample_Area_mm2 * Biome_WWF_Zone  | Habitat_Broad ),
+#                 family = poisson(), data = sb_dat, cores = 4, chains = 4,
+#                 )
 
+rich.p_zones_2 <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (log_Total_Sample_Area_mm2 * Biome_WWF_Zone  | Habitat_Broad/studyID ),
+                    family = poisson(), data = sb_dat, cores = 4, chains = 4,
+)
 
 
 save(rich.p_zones,
