d56877f9ce243307256a9c2283851d6b46556292
Emma Ladouceur <emmala@gmail.com>
2022-05-12 11:23:21 +0200

    a 3 way interaction?

---
+++ code/cluster/gsb_rich_area_i.R
@@ -0,0 +1,20 @@
+
+library(tidyverse)
+library(brms)
+
+
+path <- '/gpfs1/data/idiv_chase/emmala/Seed-Bank-Map'
+sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
+
+
+rich.p_i <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Habitat_Broad * Biome_WWF_Zone + ( 1 | studyID ),
+                family = poisson(), data = sb, cores = 4, chains = 4, iter = 2000, warmup =1000,
+                control = list(adapt_delta = 0.9))
+
+
+
+save(rich.p_i,
+     file=Sys.getenv('OFILE'))
+
+
+
