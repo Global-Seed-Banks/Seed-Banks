9c0b37b793ddefd0464842255b515b9a267c36ff
Emma Ladouceur <emmala@gmail.com>
2022-05-18 10:59:17 +0200

    zones

--- code/cluster/gsb_rich_area_zone.R
+++ code/cluster/gsb_rich_area_zone.R
@@ -19,6 +19,7 @@ sb_dat <- sb %>% filter(!is.na(Total_Species),
 
 rich.p_zones <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (1 | Method/studyID/rowID ),
                     family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 3000, warmup = 1000,
+                    control = list(adapt_delta = 0.99)
 )
 
 # rich.p_zones_2 <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (log_Total_Sample_Area_mm2 * Biome_WWF_Zone  | studyID/rowID ),
