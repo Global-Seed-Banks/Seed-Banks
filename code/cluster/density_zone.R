4834d7a17fe70995f901b2a616a74200c05e3f08
Emma Ladouceur <emmala@gmail.com>
2022-05-18 13:43:24 +0200

    organise cluster code

---
+++ code/cluster/density_zone.R
@@ -0,0 +1,32 @@
+
+library(tidyverse)
+library(brms)
+
+
+path <- '/gpfs1/data/idiv_chase/emmala/Seed-Bank-Map'
+sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
+
+# Total_Species   Seed_density_m2   Total_Seeds
+sb_dat <- sb %>% filter(!is.na(Seed_density_m2),
+                        !is.na(Total_Sample_Area_mm2)) %>%
+  # treat all random effects as factors
+  mutate( Habitat_Degraded = as.factor(Habitat_Degraded),
+          Biome_WWF_Zone = as.factor(Biome_WWF_Zone),
+          Habitat_Broad = as.factor(Habitat_Broad),
+          studyID = as.factor(studyID),
+          rowID = as.factor(rowID),
+          Method = as.factor(Method)) %>% filter(!Habitat_Broad == "Arable")
+
+
+density_zones <- brm(Seed_density_m2 ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (1 | Method/studyID/rowID ),
+                    family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
+                    control = list(adapt_delta = 0.99,
+                                   max_treedepth = 12)
+)
+
+
+save(density_zones,
+     file=Sys.getenv('OFILE'))
+
+
+
---
+++ code/cluster/seed_deg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -J seed_deg
+#SBATCH --time=16:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=4
+
+# Output files should ideally go to /work instead of /home
+#SBATCH -o /work/%u/%x-%j.out
+
+module load foss/2019b R/3.6.2-2
+export OFILE=/work/$USER/Seed-Bank-Map/$SLURM_JOB_NAME-$SLURM_JOB_ID.Rdata
+export LANG=en_US.UTF-8
+Rscript --vanilla /home/ladouceu/projects/Seed-Bank-Map/code/cluster/seed_deg.R
+
+
