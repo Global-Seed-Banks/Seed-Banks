57032c6150cc6ad5af3c9b979c8bfe5c5b0ba88e
Emma Ladouceur <emmala@gmail.com>
2022-05-23 10:28:35 +0200

    density conditional effects

--- code/mod_density.R
+++ code/mod_density.R
@@ -24,8 +24,7 @@ sb_prep <- read.csv(paste0(path2wd, 'Data/sb_prep.csv'))
 nrow(sb_prep)
 
 # remove NA values 
-sb_seeds_area <- sb_prep %>% filter(!is.na(Total_Seeds),
-                                   !is.na(Total_Sample_Area_mm2)) %>%
+sb_density_area <- sb_prep %>% filter(!is.na(Seed_density_m2)) %>%
   # treat all random effects as factors
   mutate( Habitat_Degraded = as.factor(Habitat_Degraded),
           Biome_WWF_Zone = as.factor(Biome_WWF_Zone),
@@ -33,82 +32,72 @@ sb_seeds_area <- sb_prep %>% filter(!is.na(Total_Seeds),
           studyID = as.factor(studyID),
           rowID = as.factor(rowID))
 
-nrow(sb_seeds_area)
+nrow(sb_density_area)
 
-sb_seeds_area_zone <- sb_seeds_area %>% filter(!Habitat_Broad == "Arable")
+sb_density_area_zone <- sb_density_area %>% filter(!Habitat_Broad == "Arable")
 
-nrow(sb_seeds_area_zone)
+nrow(sb_density_area_zone)
 
-
-# rich_zones <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Biome_WWF_Zone + (1 | Method/studyID/rowID ),
-#                   family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
-#                   control = list(adapt_delta = 0.99,
-#                                  max_treedepth = 12)
-# )
-
-
-# rich_habs <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Habitat_Broad + ( 1 | Method/studyID/rowID ),
-#                  family = poisson(), data = sb, cores = 4, chains = 4, iter = 3000, warmup =1000,
-#                  control = list(adapt_delta = 0.99) )
-
-# rich_deg <- brm(Total_Species ~ log_Total_Sample_Area_mm2 *  Habitat_Degraded  + ( 1 | Method/studyID/rowID ) ,
-#                 family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 3000, warmup =1000,
-#                 control = list(adapt_delta = 0.99))
-# 
 
 head(sb_prep_area)
 nrow(sb_prep_area)
 
 
+# density_zones <- brm(Seed_density_m2 ~  Biome_WWF_Zone + (1 | Method/studyID/rowID ),
+#                      family = gaussian(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
+#                      control = list(adapt_delta = 0.99,
+#                                     max_treedepth = 12)
+# )
+
 setwd(paste0(path2wd, 'Model_Fits/'))
 # models run on cluster, load in model objects here
-load( 'seed_zone.Rdata')
-load( 'seed_hab.Rdata')
-load( 'seed_deg.Rdata')
+load( 'density_zone.Rdata')
+load( 'density_hab.Rdata')
+load( 'density_deg.Rdata')
 
 
-# seeds_zones
-# seeds_habs
-# seeds_deg
+# density_zones
+# density_habs
+# density_deg
 
 # model summary
-summary(seeds_zones)
-summary(seeds_habs) 
-summary(seeds.p_i)
+summary(density_zones)
+summary(density_habs) 
+summary(density_deg)
 
 
 # posterior predictive check
 color_scheme_set("darkgray")
-pp_seed.zone <- pp_check(seeds_zones)+ xlab( "Total Seeds") + ylab("Density") +
-  labs(title= "") + xlim(0,300)+ ylim(0,0.025)+
+pp_den.zone <- pp_check(density_zones)+ xlab( "Total density") + ylab("Density") +
+  labs(title= "") +# xlim(0,300)+ ylim(0,0.025)+
   theme_classic()+  theme(legend.position= "none") # predicted vs. observed values
 
 
-pp_seed.habs <- pp_check(seeds_habs)+ xlab( "Total Seeds") + ylab("Density") +
-  labs(title= "") +  xlim(0,300)+ ylim(0,0.025)+
+pp_den.habs <- pp_check(density_habs)+ xlab( "Total density") + ylab("Density") +
+  labs(title= "") +#  xlim(0,300)+ ylim(0,0.025)+
   theme_classic()+  theme(legend.position= "bottom") # predicted vs. observed values
 
 
-pp_seed.deg <- pp_check(seeds.p_i)+ xlab( "Total Seeds") + ylab("Density") +
-  labs(title= "") +  xlim(0,300)+ ylim(0,0.025)+
+pp_den.deg <- pp_check(density_deg)+ xlab( "Total density") + ylab("Density") +
+  labs(title= "") +#  xlim(0,300)+ ylim(0,0.025)+
   theme_classic()+  theme(legend.position= "none") # predicted vs. observed values
 
-(pp_seed.zone | pp_seed.habs |  pp_seed.deg)
+(pp_den.zone | pp_den.habs |  pp_den.deg)
 
 
 # caterpillars/chains
-plot(seeds_zones)
-plot(seeds_habs)
-plot(seeds_deg)
+plot(density_zones)
+plot(density_habs)
+plot(density_deg)
 
 # # check model residuals
 # zones
-mr.zones <- residuals(seeds_zones)
+mr.zones <- residuals(density_zones)
 mr.zones <- as.data.frame(mr.zones)
 nrow(mr.zones)
 
-nrow(sb_prep_area_zone)
-zones.plot <- cbind(sb_seeds_area_zone, mr.zones$Estimate)
+nrow(sb_density_area_zone)
+zones.plot <- cbind(sb_density_area_zone, mr.zones$Estimate)
 head(zones.plot)
 #mr.zones make sure they are factors
 zones.plot$Biome_WWF_Zone <- as.factor(zones.plot$Biome_WWF_Zone )
@@ -124,11 +113,11 @@ with(zones.plot, plot(rowID, mr.zones$Estimate))
 
 
 # and habs
-mr.habs <- residuals(seeds_habs)
+mr.habs <- residuals(density_habs)
 mr.habs <- as.data.frame(mr.habs)
 nrow(mr.habs)
-nrow(sb_prep_area)
-habs.plot <- cbind(sb_seeds_area, mr.habs$Estimate)
+nrow(sb_density_area)
+habs.plot <- cbind(sb_density_area, mr.habs$Estimate)
 habs.plot$Habitat_Broad <- as.factor(habs.plot$Habitat_Broad )
 habs.plot$Method <- as.factor(habs.plot$Method )
 habs.plot$studyID <- as.factor(habs.plot$studyID )
@@ -141,11 +130,11 @@ with(habs.plot, plot(studyID, mr.habs$Estimate))
 with(habs.plot, plot(rowID, mr.habs$Estimate))
 
 # and deg
-mr.deg <- residuals(seeds_deg)
+mr.deg <- residuals(density_deg)
 mr.deg <- as.data.frame(mr.deg)
 nrow(mr.deg)
-nrow(sb_prep_area)
-deg.plot <- cbind(sb_seeds_area, mr.deg$Estimate)
+nrow(sb_density_area)
+deg.plot <- cbind(sb_density_area, mr.deg$Estimate)
 deg.plot$Habitat_Degraded <- as.factor(deg.plot$Habitat_degraded )
 deg.plot$Method <- as.factor(deg.plot$Method )
 deg.plot$studyID <- as.factor(deg.plot$studyID )
@@ -159,239 +148,111 @@ with(deg.plot, plot(rowID, mr.deg$Estimate))
 
 head(sb_prep_area)
 
-# WWF ZONES model
-# # for plotting fixed effects
-seeds_zone_fitted <- cbind(seeds_zones$data,
-                          fitted(seeds_zones, re_formula = NA
-                          )) %>%
-  as_tibble() %>% inner_join(sb_seeds_area_zone %>% select(Total_Species, 
-                                                          Total_Number_Samples, Total_Sample_Area_mm2,
-                                                          log_Total_Sample_Area_mm2,
-                                                          Biome_WWF_Zone),
-                             #  by= c("Total_Species2","Biome_WWF_Zone","Habitat_Broad","studyID", "rowID")
-  )
 
 
-head(seeds_zone_fitted)
-nrow(seeds_zone_fitted)
+density_zone_c <- conditional_effects(density_zones, effects = 'Biome_WWF_Zone', re_formula = NA, method = 'fitted')  # conditional effects
 
-seeds_habs_fitted <- cbind(seeds_habs$data,
-                          fitted(seeds_habs, re_formula = NA
-                          )) %>%
-  as_tibble() %>% inner_join(sb_seeds_area %>% select(Total_Species, 
-                                                     Total_Number_Samples, Total_Sample_Area_mm2,
-                                                     log_Total_Sample_Area_mm2,
-                                                     Habitat_Broad),
-                             #  by= c("Total_Species2","Biome_WWF_Zone","Habitat_Broad","studyID", "rowID")
-  )
+head(density_zone_c)
 
 
-head(seeds_habs_fitted)
-nrow(seeds_habs_fitted)
+Density_Zone_Fig <- ggplot() + 
+  geom_hline(yintercept = 0,linetype="longdash") +
+  geom_point(data = sb_density_area_zone,
+             aes(x = Biome_WWF_Zone, y = Seed_density_m2, #colour = 	"#C0C0C0"
+                 colour = Biome_WWF_Zone
+                 ), 
+             size = 0.25, alpha = 0.2, position = position_jitter(width = 0.05, height=0.45)) +
+  geom_point(data = density_zone_c$Biome_WWF_Zone,
+             aes(x = Biome_WWF_Zone, y = estimate__, colour = Biome_WWF_Zone), size = 3) +
+  geom_errorbar(data = density_zone_c$Biome_WWF_Zone,
+                aes(x = Biome_WWF_Zone, ymin = lower__, ymax = upper__, colour = Biome_WWF_Zone),
+                size = 1, width = 0) +
+  labs(x = '',
+       y = expression(paste('Seed density (',m^2,')')),
+       subtitle= 'WWF Zones') +
+ # scale_color_manual(values =  c(	"#C0C0C0","#228B22", 	"#6B8E23"))  + 
+  scale_color_viridis(discrete = T, option="D")  +
+  scale_fill_viridis(discrete = T, option="D")  +
+  #ylim(0,100000)+
+   coord_cartesian( ylim = c(0,40000)) +
+  theme_bw(base_size=14)+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = 0.2, unit = "cm"),
+                               plot.title=element_text(size=18, hjust=0.5),
+                               strip.background = element_blank(),legend.position="none") 
 
 
-summary(seeds_deg)
-
-seeds_deg_fitted <- cbind(seeds.p_i$data,
-                         fitted(seeds.p_i, re_formula = NA
-                         )) %>%
-  as_tibble() %>% inner_join(sb_seeds_area %>% select(Total_Species, 
-                                                     Total_Number_Samples, Total_Sample_Area_mm2,
-                                                     log_Total_Sample_Area_mm2,
-                                                     Habitat_Degraded),
-                             #  by= c("Total_Species2","Biome_WWF_Zone","Habitat_Broad","studyID", "rowID")
-  )
-
-
-head(seeds_deg_fitted)
-nrow(seeds_deg_fitted)
+Density_Zone_Fig
 
 
 
-# fixed effect coefficients
-seeds_zone_fixef <- fixef(seeds_zones)
-head(seeds_zone_fixef)
+density_hab_c <- conditional_effects(density_habs, effects = 'Habitat_Broad', re_formula = NA, method = 'fitted')  # conditional effects
 
-# Random effect coefficients
-seeds_zone_coef <- coef(seeds_zones)
-seeds_zone_coef # dont really need this
+head(density_hab_c)
 
 
-
-# fixed effect coefficients
-seeds_habs_fixef <- fixef(seeds_habs)
-head(seeds_habs_fixef)
-
-# Random effect coefficients
-seeds_habs_coef <- coef(seeds_habs)
-seeds_habs_coef
-
-
-
-# fixed effect coefficients
-seeds_deg_fixef <- fixef(seeds.p_i)
-head(seeds_deg_fixef)
-
-# Random effect coefficients
-seeds_deg_coef <- coef(seeds.p_i)
-seeds_deg_coef
-
-setwd(paste0(path2wd, 'Data/'))
-# # # save data objects to avoid doing this every time
-save(seeds_zone_fitted, seeds_zone_fixef, seeds_zone_coef, file = 'seeds_zone.mod_dat.Rdata')
-
-
-setwd(paste0(path2wd, 'Data/'))
-save(seeds_habs_fitted, seeds_habs_fixef, seeds_habs_coef, file = 'seeds_habs.mod_dat.Rdata')
-
-
-setwd(paste0(path2wd, 'Data/'))
-save(seeds_deg_fitted, seeds_deg_fixef, seeds_deg_fixef, file = 'seeds_deg.mod_dat.Rdata')
-
-
-# plots
-setwd(paste0(path2wd, 'Data/'))
-#load('seeds.area.poisson.mod_dat.Rdata')
-load('seeds_zone.mod_dat.Rdata')
-
-# plot seedsness area zone relationship
-
-sb_prep_area_zone %>% summarise
-
-fig_seeds.zone <- ggplot() + 
-  #facet_wrap(~Biome_WWF_Zone, scales="free") +
-  # horizontal zero line
-  geom_hline(yintercept = 0, lty = 2) +
-  # raw data points
-  geom_point(data = sb_seeds_area_zone ,
-             aes(x = (Total_Sample_Area_mm2/1000000),
-                 # x = Total_Sample_Area_mm2,
-                 y = Total_Seeds, colour = Biome_WWF_Zone,
+Density_Hab_Fig <- ggplot() + 
+  geom_hline(yintercept = 0,linetype="longdash") +
+  geom_point(data = sb_density_area,
+             aes(x = Habitat_Broad, y = Seed_density_m2, #colour = 	"#C0C0C0"
+                 colour = Habitat_Broad
              ), 
-             size = 1.2, shape=1, position = position_jitter(width = 0.25, height=2.5)) +
-  # fixed effect
-  geom_line(data = seeds_zone_fitted,
-            aes(x = (Total_Sample_Area_mm2/1000000), 
-                # x = Total_Sample_Area_mm2,
-                y = Estimate, colour = Biome_WWF_Zone),
-            size = 1) +
-  # uncertainy in fixed effect
-  geom_ribbon(data = seeds_zone_fitted,
-              aes( x =  (Total_Sample_Area_mm2/1000000), 
-                   #x =  Total_Sample_Area_mm2, 
-                   ymin = Q2.5, ymax = Q97.5, fill = Biome_WWF_Zone),
-              alpha = 0.3 ) +
+             size = 0.25, alpha = 0.2, position = position_jitter(width = 0.05, height=0.45)) +
+  geom_point(data = density_hab_c$Habitat_Broad,
+             aes(x = Habitat_Broad, y = estimate__, colour = Habitat_Broad), size = 3) +
+  geom_errorbar(data = density_hab_c$Habitat_Broad,
+                aes(x = Habitat_Broad, ymin = lower__, ymax = upper__, colour = Habitat_Broad),
+                size = 1, width = 0) +
+  labs(x = '',
+       y = expression(paste('Seed density (',m^2,')')),
+       subtitle= 'Habitats') +
+  # scale_color_manual(values =  c(	"#C0C0C0","#228B22", 	"#6B8E23"))  + 
   scale_color_viridis(discrete = T, option="D")  +
   scale_fill_viridis(discrete = T, option="D")  +
-  theme_bw(base_size=14 ) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), strip.background = element_rect(colour="black", fill="white"),
-                                  legend.position="bottom") +
-  labs(y = "Total Seeds",  x = expression(paste('Total Sample Area ' , m^2)),
-       color = "WWF Zone", fill = "WWF Zone", subtitle= "") + guides(col = guide_legend(ncol = 2)) +
-#xlim(0,800)+ 
-#  coord_cartesian( ylim = c(0,530000))# +
-scale_x_log10() + scale_y_log10() 
-
-fig_seeds.zone
-
-# (Total_Sample_Area_mm2/1000000) =m2
-
-# Habitat
+  #ylim(0,100000)+
+  coord_cartesian( ylim = c(0,40000)) +
+  theme_bw(base_size=14)+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = 0.2, unit = "cm"),
+                               plot.title=element_text(size=18, hjust=0.5),
+                               strip.background = element_blank(),legend.position="none") 
 
 
-setwd(paste0(path2wd, 'Data/'))
-load('seeds_habs.mod_dat.Rdata')
+Density_Hab_Fig
 
-# plot seedsness area relationship
-colnames(sb_prep_area)
 
-fig_seeds.habs <- ggplot() + 
-  # facet_wrap(~Habitat_Broad, scales="free") +
-  # horizontal zero line
-  geom_hline(yintercept = 0, lty = 2) +
-  # raw data points
-  geom_point(data = sb_seeds_area ,
-             aes(x = (Total_Sample_Area_mm2/1000000),
-                 # x = Total_Sample_Area_mm2,
-                 y = Total_Seeds,
-                 colour = Habitat_Broad),
-             size = 1.2, shape=1, position = position_jitter(width = 0.25, height=2.5)) +
-  # fixed effect
-  geom_line(data = seeds_habs_fitted,
-            aes(x = (Total_Sample_Area_mm2/1000000), 
-                # x = Total_Sample_Area_mm2,
-                y = Estimate, colour = Habitat_Broad),
-            size = 1) +
-  # uncertainy in fixed effect
-  geom_ribbon(data = seeds_habs_fitted,
-              aes( x =  (Total_Sample_Area_mm2/1000000), 
-                   #x =  Total_Sample_Area_mm2, 
-                   ymin = Q2.5, ymax = Q97.5,  fill = Habitat_Broad),
-              alpha = 0.3) +
-  #coord_cartesian(xlim = c(min(sb_prep_area$Total_Sample_Area_mm2), quantile(sb_prep_area$Total_Sample_Area_mm2, 0.97))) +
-  # xlim(0,10) + ylim(0,200) +
+
+
+density_deg_c <- conditional_effects(density_deg, effects = 'Habitat_Degraded', re_formula = NA, method = 'fitted')  # conditional effects
+
+head(density_deg_c)
+
+
+Density_Deg_Fig <- ggplot() + 
+  geom_hline(yintercept = 0,linetype="longdash") +
+  geom_point(data = sb_density_area,
+             aes(x = Habitat_Degraded, y = Seed_density_m2, #colour = 	"#C0C0C0"
+                 colour = Habitat_Degraded
+             ), 
+             size = 0.25, alpha = 0.2, position = position_jitter(width = 0.05, height=0.45)) +
+  geom_point(data = density_deg_c$Habitat_Degraded,
+             aes(x = Habitat_Degraded, y = estimate__, colour = Habitat_Degraded), size = 3) +
+  geom_errorbar(data = density_deg_c$Habitat_Degraded,
+                aes(x = Habitat_Degraded, ymin = lower__, ymax = upper__, colour = Habitat_Degraded),
+                size = 1, width = 0) +
+  labs(x = '',
+       y = expression(paste('Seed density (',m^2,')')),
+       subtitle= 'Habitats') +
+  # scale_color_manual(values =  c(	"#C0C0C0","#228B22", 	"#6B8E23"))  + 
   scale_color_viridis(discrete = T, option="D")  +
   scale_fill_viridis(discrete = T, option="D")  +
-  theme_bw(base_size=14 ) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), strip.background = element_rect(colour="black", fill="white"),
-                                  legend.position="bottom") +
-  labs(y = "",  x = expression(paste('Total Sample Area ' , m^2)),
-       color = "Habitats", fill = "Habitats", subtitle = "") +  guides(col = guide_legend(ncol = 2)) +
-# xlim(0,800)+ ylim(0,200)+
-scale_x_log10() + scale_y_log10() 
-
-fig_seeds.habs
+  #ylim(0,100000)+
+  coord_cartesian( ylim = c(0,40000)) +
+  theme_bw(base_size=14)+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = 0.2, unit = "cm"),
+                               plot.title=element_text(size=18, hjust=0.5),
+                               strip.background = element_blank(),legend.position="none") 
 
 
-setwd(paste0(path2wd, 'Data/'))
-load('seeds_deg.mod_dat.Rdata')
-
-# plot seedsness area relationship
-colnames(sb_prep_area)
-
-fig_seeds.deg <- ggplot() + 
-  # facet_wrap(~Habitat_Broad, scales="free") +
-  # horizontal zero line
-  geom_hline(yintercept = 0, lty = 2) +
-  # raw data points
-  geom_point(data = sb_seeds_area ,
-             aes(x = (Total_Sample_Area_mm2/1000000),
-                 # x = Total_Sample_Area_mm2,
-                 y = Total_Seeds,
-                 colour = Habitat_Degraded),
-             size = 1.2, shape=1, position = position_jitter(width = 0.25, height=2.5)) +
-  # fixed effect
-  geom_line(data = seeds_deg_fitted,
-            aes(x = (Total_Sample_Area_mm2/1000000), 
-                # x = Total_Sample_Area_mm2,
-                y = Estimate, colour = Habitat_Degraded),
-            size = 1) +
-  # uncertainy in fixed effect
-  geom_ribbon(data = seeds_deg_fitted,
-              aes( x =  (Total_Sample_Area_mm2/1000000), 
-                   #x =  Total_Sample_Area_mm2, 
-                   ymin = Q2.5, ymax = Q97.5,  fill = Habitat_Degraded),
-              alpha = 0.3) +
-  #coord_cartesian(xlim = c(min(sb_prep_area$Total_Sample_Area_mm2), quantile(sb_prep_area$Total_Sample_Area_mm2, 0.97))) +
-  # xlim(0,10) + ylim(0,200) +
-  scale_color_viridis(discrete = T, option="D")  +
-  scale_fill_viridis(discrete = T, option="D")  +
-  theme_bw(base_size=14 ) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), strip.background = element_rect(colour="black", fill="white"),
-                                  legend.position="bottom") +
-  labs(y = "",  x = expression(paste('Total Sample Area ' , m^2)),
-       color = "Degraded Habitat", fill = "Degraded Habitat", subtitle = "")+  guides(col = guide_legend(ncol = 2)) +
-# xlim(0,800)+ ylim(0,200)+
-scale_x_log10() + scale_y_log10() 
-
-fig_seeds.deg
+Density_Deg_Fig
 
 
-Almost_Total_Seeds_Fig <- (fig_seeds.zone | fig_seeds.habs | fig_seeds.deg)
-
-
-Total_Seeds_Fig <- (Almost_Total_Seeds_Fig) + plot_annotation(title = "Total Seeds",
-                  theme = theme(plot.title = element_text(hjust = 0.5, size= 18))) + plot_layout(ncol=3)
-
-
-Total_Seeds_Fig
-
-
-(Total_Species_Fig / Total_Seeds_Fig)
+Density_Zone_Fig | Density_Hab_Fig | Density_Deg_Fig
