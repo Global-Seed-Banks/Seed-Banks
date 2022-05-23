08139389c11980959615a1f66e1edc00b2af94e4
Emma Ladouceur <emmala@gmail.com>
2022-05-23 09:44:04 +0200

    attempting centreing log area for richness

--- code/cluster/rich_hab.R
+++ code/cluster/rich_hab.R
@@ -9,7 +9,7 @@ sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.st
 # Total_Species   Seed_density_m2   Total_Seeds
 sb_dat <- sb %>% 
   filter(!is.na(Total_Species),
-                        !is.na(Total_Sample_Area_mm2)) %>%
+                        !is.na(Centred_log_Total_Sample_Area_m2)) %>%
   # treat all random effects as factors
   mutate( Habitat_Degraded = as.factor(Habitat_Degraded),
           Biome_WWF_Zone = as.factor(Biome_WWF_Zone),
@@ -18,7 +18,7 @@ sb_dat <- sb %>%
           rowID = as.factor(rowID),
           Method = as.factor(Method)) #%>% filter(!Habitat_Broad == "Arable")
 
-rich_habs <- brm(Total_Species ~ log_Total_Sample_Area_mm2 * Habitat_Broad + ( 1 | Method/studyID/rowID ),
+rich_habs <- brm(Total_Species ~ Centred_log_Total_Sample_Area_m2 * Habitat_Broad + ( 1 | Method/studyID/rowID ),
                     family = poisson(), data = sb, cores = 4, chains = 4, iter = 3000, warmup =1000,
                     control = list(adapt_delta = 0.99) )
 
