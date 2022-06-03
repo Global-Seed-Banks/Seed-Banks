3bd7b48bc4021920efaffe3f4660856fa0c2c108
Emma Ladouceur <emmala@gmail.com>
2022-06-03 10:52:53 +0200

    broad

--- code/cluster/density_biome_broad.R
+++ code/cluster/density_biome_broad.R
@@ -4,19 +4,17 @@ library(brms)
 
 
 path <- '/gpfs1/data/idiv_chase/emmala/Seed-Bank-Map'
-sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
+sb <- read.csv(paste0(path, '/sb_prep_broad.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
 
 # Total_Species   Seed_density_m2   Total_Seeds
 sb_dat <- sb %>% filter(!is.na(Seed_density_m2)) %>%
   # treat all random effects as factors
   mutate( Habitat_Degraded = as.factor(Habitat_Degraded),
-          # Biome_WWF_Broad = as.factor(Biome_WWF_Broad),
-          # Habitat_Broad = as.factor(Habitat_Broad),
+          Biome_Broad_Hab = as.factor(Biome_Broad_Hab),
+          Habitat_Broad = as.factor(Habitat_Broad),
           studyID = as.factor(studyID),
           rowID = as.factor(rowID),
-          Method = as.factor(Method)) %>% 
-  mutate(Biome_Broad_Hab = case_when(Habitat_Broad %in% c("Arable", "Aquatic") ~ Habitat_Broad ,
-                                     TRUE ~ Biome_WWF_Broad))
+          Method = as.factor(Method)) 
 
 
 density_biome_broad <- brm(Seed_density_m2 ~  Biome_Broad_Hab + (1 | Method/studyID/rowID ),
--- code/cluster/seed_biome_broad.R
+++ code/cluster/seed_biome_broad.R
@@ -4,20 +4,18 @@ library(brms)
 
 
 path <- '/gpfs1/data/idiv_chase/emmala/Seed-Bank-Map'
-sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
+sb <- read.csv(paste0(path, '/sb_prep_broad.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
 
 # Total_Species   Seed_density_m2   Total_Seeds
 sb_dat <- sb %>% filter(!is.na(Total_Seeds),
                         !is.na(Centred_log_Total_Sample_Area_m2)) %>%
   # treat all random effects as factors
   mutate( Habitat_Degraded = as.factor(Habitat_Degraded),
-          # Biome_WWF_Zone = as.factor(Biome_WWF_Zone),
-          # Habitat_Broad = as.factor(Habitat_Broad),
+          Biome_Broad_Hab = as.factor(Biome_Broad_Hab),
+          Habitat_Broad = as.factor(Habitat_Broad),
           studyID = as.factor(studyID),
           rowID = as.factor(rowID),
-          Method = as.factor(Method)) %>% 
-  mutate(Biome_Broad_Hab = case_when(Habitat_Broad %in% c("Arable", "Aquatic") ~ Habitat_Broad ,
-                                     TRUE ~ Biome_WWF_Broad))
+          Method = as.factor(Method)) 
 
 
 seeds_biome_broad <- brm(Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + (1 | Method/studyID/rowID ),
