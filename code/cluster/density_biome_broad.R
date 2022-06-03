f31fa2140b5afdf9b8423c65d65bd182a9ef15b9
Emma Ladouceur <emmala@gmail.com>
2022-06-03 09:49:08 +0200

    broad biomes

--- code/cluster/density_biome_broad.R
+++ code/cluster/density_biome_broad.R
@@ -7,8 +7,7 @@ path <- '/gpfs1/data/idiv_chase/emmala/Seed-Bank-Map'
 sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
 
 # Total_Species   Seed_density_m2   Total_Seeds
-sb_dat <- sb %>% filter(!is.na(Total_Species),
-                        !is.na(Centred_log_Total_Sample_Area_m2)) %>%
+sb_dat <- sb %>% filter(!is.na(Seed_density_m2)) %>%
   # treat all random effects as factors
   mutate( Habitat_Degraded = as.factor(Habitat_Degraded),
           Biome_WWF_Zone = as.factor(Biome_WWF_Zone),
@@ -18,14 +17,15 @@ sb_dat <- sb %>% filter(!is.na(Total_Species),
           rowID = as.factor(rowID),
           Method = as.factor(Method)) #%>% filter(!Habitat_Broad == "Arable")
 
-rich_biome_broad <- brm(Total_Species ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + (1 | Method/studyID/rowID ),
-                    family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
-                    control = list(adapt_delta = 0.9999,
-                                   max_treedepth = 13)
+
+density_biome_broad <- brm(Seed_density_m2 ~  Biome_Broad_Hab + (1 | Method/studyID/rowID ),
+                    family = student(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
+                    control = list(adapt_delta = 0.999,
+                                   max_treedepth = 12)
 )
 
 
-save(rich_biome_broad,
+save(density_biome_broad,
      file=Sys.getenv('OFILE'))
 
 
