36ee4be2778618ead3be39169c3b093f91fccd7c
Emma Ladouceur <emmala@gmail.com>
2022-11-09 10:31:36 +0100

    predicted diversity across all the scales

--- code/Predicted_Beta.R
+++ code/Predicted_Beta.R
@@ -46,21 +46,21 @@ load( 'seed_m2.Rdata')
 
 summary(sb_biome_area)
 
-sb_biome_area_prep <- sb_biome_area %>% distinct(Total_Sample_Area_mm2, Total_Sample_Area_m2, Centred_log_Total_Sample_Area_m2) %>%
-  mutate(Total_Sample_Area_mm2 = round(Total_Sample_Area_mm2, 2),
-         Total_Sample_Area_m2 = round(Total_Sample_Area_m2, 6),
-         Centred_log_Total_Sample_Area_m2 = round(Centred_log_Total_Sample_Area_m2, 6),
-         ) %>% arrange(Total_Sample_Area_m2) #
-
-summary(sb_biome_area_prep)
-View(sb_biome_area_prep %>% distinct(Total_Sample_Area_m2) %>%
-       filter( Total_Sample_Area_m2 >=8.210905 &  Total_Sample_Area_m2 <16) )
-
-sb_biome_area_prep %>%
-  summarise(Total_Sample_Area_m2 = seq(0.010000, 12.311357, length.out = 10 ) ) 
-              
-sb_biome_area_prep %>% filter( Total_Sample_Area_m2 == 0.010000)
-sb_biome_area_prep %>% filter( Total_Sample_Area_m2 == 15.000000)
+# sb_biome_area_prep <- sb_biome_area %>% distinct(Total_Sample_Area_mm2, Total_Sample_Area_m2, Centred_log_Total_Sample_Area_m2) %>%
+#   mutate(Total_Sample_Area_mm2 = round(Total_Sample_Area_mm2, 2),
+#          Total_Sample_Area_m2 = round(Total_Sample_Area_m2, 6),
+#          Centred_log_Total_Sample_Area_m2 = round(Centred_log_Total_Sample_Area_m2, 6),
+#          ) %>% arrange(Total_Sample_Area_m2) #
+# 
+# summary(sb_biome_area_prep)
+# View(sb_biome_area_prep %>% distinct(Total_Sample_Area_m2) %>%
+#        filter( Total_Sample_Area_m2 >=8.210905 &  Total_Sample_Area_m2 <16) )
+# 
+# sb_biome_area_prep %>%
+#   summarise(Total_Sample_Area_m2 = seq(0.010000, 12.311357, length.out = 10 ) ) 
+#               
+# sb_biome_area_prep %>% filter( Total_Sample_Area_m2 == 0.010000)
+# sb_biome_area_prep %>% filter( Total_Sample_Area_m2 == 15.000000)
 
 
 rich_biome_predict <- sb_biome_area %>% 
@@ -84,6 +84,15 @@ rich_biome_predict_df <- rich_biome_predict  %>%
 
 View(head(rich_biome_predict_df))
 
+setwd(paste0(path2wd, 'Data/'))
+write.csv(rich_biome_predict_df,  "rich_biome_predict_df.csv")
+
+rm(list = ls())
+
+rich_biome_predict_df <- read.csv(paste0(path2wd, 'Data/rich_biome_predict_df.csv'))
+
+
+
 head(rich_biome_predict_df)
 colnames(rich_biome_predict_df)
 View(rich_biome_predict_df %>% distinct(Total_Sample_Area_m2))
@@ -91,21 +100,35 @@ View(rich_biome_predict_df %>% distinct(Total_Sample_Area_m2))
 # 0.010000
 # 15.000000
 
+nrow(rich_biome_predict_df)
+
 rich_biome_a <- rich_biome_predict_df %>%
   select(-c(.draw, .row, .chain, .iteration, Centred_log_Total_Sample_Area_m2, Biome_Broad_Hab_group)) %>%
   filter( Total_Sample_Area_m2 == 0.010000   ) %>% # Yang et al
+  group_by(Biome_Broad_Hab) %>%
+   filter(
+     predicted > quantile(predicted, probs=0.025),
+     predicted < quantile(predicted, probs=0.975),
+  ) %>% sample_n(1000)  %>%
   mutate(a_samp_scale = Total_Sample_Area_m2,
          a_predicted = predicted) %>%
-  select(-c(Total_Sample_Area_m2,predicted ))
-  
+  select(-c(Total_Sample_Area_m2,predicted, X ))
+
+nrow(rich_biome_a)
 
 rich_biome_g <- rich_biome_predict_df %>%
   select(-c(.draw, .row, .chain, .iteration, Centred_log_Total_Sample_Area_m2, Biome_Broad_Hab_group)) %>%
   filter(  Total_Sample_Area_m2 ==  15.000000   ) %>% # arbitrary gamma scale
+  group_by(Biome_Broad_Hab) %>%
+  filter(
+    predicted > quantile(predicted, probs=0.025),
+    predicted < quantile(predicted, probs=0.975),
+  ) %>% sample_n(1000)  %>%
   mutate(g_samp_scale = Total_Sample_Area_m2,
          g_predicted = predicted) %>%
-  select(-c(Total_Sample_Area_m2,predicted ))
+  select(-c(Total_Sample_Area_m2,predicted, X))
 
+head(rich_biome_g)
 
 rich_biome_scales <- rich_biome_a %>% left_join(rich_biome_g) %>%
   mutate(b_predicted = (g_predicted/a_predicted))
@@ -115,49 +138,71 @@ head(rich_biome_scales)
 setwd(paste0(path2wd, 'Data/'))
 write.csv(rich_biome_scales,  "sb_av_div_scales.csv")
 
+rm(list = ls())
+
+rich_biome_scales <- read.csv(paste0(path2wd, 'Data/sb_av_div_scales.csv'))
+
+
+View(rich_biome_scales %>% filter(Biome_Broad_Hab ==  "Boreal Forests/Taiga"))
 
 
 rich_biome_div <- rich_biome_scales %>%
-  group_by(Biome_Broad_Hab_group, Centred_log_Total_Sample_Area_m2, #Total_Sample_Area_mm2,
-           Total_Sample_Area_m2) %>%
-  mutate( Estimate = mean(predicted, na.rm =TRUE ),
-          `Upper CI` = quantile(predicted, probs=0.975, na.rm =TRUE ),
-          `Lower CI` = quantile(predicted, probs=0.025, na.rm =TRUE )
+  group_by(Biome_Broad_Hab) %>%
+  mutate( a_Estimate = mean(a_predicted, na.rm =TRUE ),
+          `a_Upper CI` = quantile(a_predicted, probs=0.975, na.rm =TRUE ),
+          `a_Lower CI` = quantile(a_predicted, probs=0.025, na.rm =TRUE ),
+           g_Estimate = mean(g_predicted, na.rm =TRUE ),
+          `g_Upper CI` = quantile(g_predicted, probs=0.975, na.rm =TRUE ),
+          `g_Lower CI` = quantile(g_predicted, probs=0.025, na.rm =TRUE ),
+           b_Estimate = mean(b_predicted, na.rm =TRUE ),
+          `b_Upper CI` = quantile(b_predicted, probs=0.975, na.rm =TRUE ),
+          `b_Lower CI` = quantile(b_predicted, probs=0.025, na.rm =TRUE ),
   ) %>% 
-  select(-predicted) %>% distinct() %>% ungroup() %>%
-  mutate(scale = "alpha")
-
-rich_biome_fit_g <- rich_biome_predict_df %>%
-  select(-c(.draw, .row, .chain, .iteration)) %>%
-  filter( Total_Sample_Area_m2 == 15.000000) %>%
-  group_by(Biome_Broad_Hab_group, Centred_log_Total_Sample_Area_m2, #Total_Sample_Area_mm2,
-           Total_Sample_Area_m2) %>%
-  mutate( Estimate = mean(predicted, na.rm =TRUE ),
-          `Upper CI` = quantile(predicted, probs=0.975, na.rm =TRUE ),
-          `Lower CI` = quantile(predicted, probs=0.025, na.rm =TRUE )
-  ) %>% 
-  select(-predicted) %>% distinct() %>% ungroup() %>%
-  mutate(scale = "gamma")
-
-rich_biome_fit <- rich_biome_fit_a %>% bind_rows(rich_biome_fit_g)
+  select(-c(X, a_predicted, g_predicted, b_predicted)) %>% distinct() %>% ungroup()
 
 
-View(rich_biome_fit)
-setwd('~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/Model_Extract/')
-save(s.loss.mm, file = 'fitted_s.loss_compare.Rdata')
-load('~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/Model_Extract/fitted_s.loss_compare.Rdata')
+head(rich_biome_div)
+View(rich_biome_div)
 
-setwd(paste0(path2wd, 'Tables/'))
-write.csv(rich_biome_fit.mm, "table_4.csv")
+setwd(paste0(path2wd, 'Data/'))
+write.csv(rich_biome_div,  "sb_av_div_estimates.csv")
 
-rich_biome_i <- ggplot() + 
+rich_biome_div <- read.csv(paste0(path2wd, 'Data/sb_av_div_estimates.csv'))
+
+
+predicted_points<- rich_biome_scales %>%  
+  group_by(Biome_Broad_Hab) %>%
+#   filter(a_predicted > quantile(a_predicted, probs=0.025),
+#                                a_predicted < quantile(a_predicted, probs=0.975),
+#                                g_predicted > quantile(g_predicted, probs=0.025),
+#                                g_predicted < quantile(g_predicted, probs=0.975),
+#                                b_predicted > quantile(a_predicted, probs=0.025),
+#                                b_predicted < quantile(b_predicted, probs=0.975),
+#                                # take 50 samples- this will be uncertainty represented in figure 4
+# ) %>% 
+  sample_n(200)  %>% select(-X)
+
+
+View(predicted_points)
+
+setwd(paste0(path2wd, 'Data/'))
+write.csv(predicted_points,  "predicted_samps.csv")
+
+predicted_points <- read.csv(paste0(path2wd, 'Data/predicted_samps.csv'))
+
+
+
+rich_biome_a <- ggplot() + 
   geom_hline(yintercept = 0,linetype="longdash") +
-  facet_wrap(.~scale) +
-  geom_point(data = rich_biome_fit,
-             aes(x = Biome_Broad_Hab , y = Estimate, colour = Biome_Broad_Hab), 
+  geom_point(data = predicted_points,
+             aes(x = Biome_Broad_Hab , y = a_predicted, colour = Biome_Broad_Hab),
+             #position = position_dodge(width = 0.75),
+             position = position_jitter(width = 0.3, height=0.5), size = 1, alpha = 0.2) +
+  geom_point(data = rich_biome_div,
+             aes(x = Biome_Broad_Hab , y = a_Estimate, colour = Biome_Broad_Hab), 
              position = position_dodge(width = 0.75), size = 3) +
-  geom_errorbar(data = rich_biome_fit,
-                aes(x = Biome_Broad_Hab , ymin = `Lower CI`, ymax =  `Upper CI`, colour = Biome_Broad_Hab),
+  geom_errorbar(data = rich_biome_div,
+                aes(x = Biome_Broad_Hab , ymin = `a_Lower CI`, ymax =  `a_Upper CI`, colour = Biome_Broad_Hab),
                 position = position_dodge(width = 0.75),
                 size = 0.75, width = 0) +
   scale_color_viridis(discrete = T, option="D")  +
@@ -168,44 +213,64 @@ rich_biome_i <- ggplot() +
                                plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = 0.2, unit = "cm"),
                                plot.title=element_text(size=18, hjust=0.5),
                                strip.background = element_blank(),legend.position="none") + 
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) + 
- # coord_cartesian( ylim = c(0,150)) +
+  #scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) + 
+  # coord_cartesian( ylim = c(0,90)) +
+  ggtitle((expression(paste(italic(alpha), '-scale (0.01' ,m^2,')', sep = ''))))+
   labs(x='',
-       y = 'Average species richness in the soil seed bank',
+       y = 'Average species richness',
+  ) 
+
+
+rich_biome_a
+
+
+rich_biome_g <- ggplot() + 
+  geom_hline(yintercept = 0,linetype="longdash") +
+  #facet_wrap(.~scale) +
+  geom_point(data = predicted_points,
+             aes(x = Biome_Broad_Hab , y = g_predicted, colour = Biome_Broad_Hab),
+             #position = position_dodge(width = 0.75),
+             position = position_jitter(width = 0.3, height=0.35), size = 1, alpha = 0.2) +
+  geom_point(data = rich_biome_div,
+             aes(x = Biome_Broad_Hab , y = g_Estimate, colour = Biome_Broad_Hab), 
+             position = position_dodge(width = 0.75), size = 3) +
+  geom_errorbar(data = rich_biome_div,
+                aes(x = Biome_Broad_Hab , ymin = `g_Lower CI`, ymax =  `g_Upper CI`, colour = Biome_Broad_Hab),
+                position = position_dodge(width = 0.75),
+                size = 0.75, width = 0) +
+  scale_color_viridis(discrete = T, option="D")  +
+  # scale_shape_manual(name = "Average total species",
+  #                    values = c(16, 17), labels = c("Min area", "Max area") )+ 
+  theme_bw(base_size=18)+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               axis.text.x=element_blank(),
+                               plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = 0.2, unit = "cm"),
+                               plot.title=element_text(size=18, hjust=0.5),
+                               strip.background = element_blank(),legend.position="none") + 
+  #scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) + 
+ # coord_cartesian( ylim = c(0,90)) +
+  ggtitle((expression(paste(italic(gamma), '-scale (15' ,m^2,')', sep = ''))))+
+  labs(x='',
+       y = 'Average species richness',
        ) 
 
+#(g/' ,m^2, '/year)'
 
-rich_biome_i
+rich_biome_g
 
-
-fit_a <- rich_biome_fit_a %>% select(Biome_Broad_Hab, Estimate,  `Lower CI` , `Upper CI`) %>%
-  mutate(Estimate_a = Estimate,
-         `Lower CI a` = `Lower CI`,
-         `Upper CI a` = `Upper CI`
-  ) %>% select(-c(Estimate,  `Lower CI` , `Upper CI`)) 
-
-fit_g <- rich_biome_fit_g %>% select(Estimate,  `Lower CI` , `Upper CI`) %>%
-  mutate(Estimate_g = Estimate,
-         `Lower CI g` = `Lower CI`,
-         `Upper CI g` = `Upper CI`
-  )  %>% select(-c(Estimate,  `Lower CI` , `Upper CI`)) 
-
-
-fit_b <- fit_a %>% bind_cols(fit_g) %>%
-  mutate( Estimate_b = Estimate_g/Estimate_a,
-          `Lower CI b` = `Lower CI g`/ `Lower CI a`,
-          `Upper CI b` = `Upper CI g`/`Upper CI a`,
-          )
 
 
 rich_biome_b <- ggplot() + 
   geom_hline(yintercept = 0,linetype="longdash") +
   #facet_wrap(.~scale) +
-  geom_point(data = fit_b,
-             aes(x = Biome_Broad_Hab , y = Estimate_b, colour = Biome_Broad_Hab), 
+  geom_point(data = predicted_points,
+             aes(x = Biome_Broad_Hab , y = b_predicted, colour = Biome_Broad_Hab),
+             #position = position_dodge(width = 0.75),
+             position = position_jitter(width = 0.3, height=0.5), size = 1, alpha = 0.2) +
+  geom_point(data = rich_biome_div,
+             aes(x = Biome_Broad_Hab , y = b_Estimate, colour = Biome_Broad_Hab), 
              position = position_dodge(width = 0.75), size = 3) +
-  geom_errorbar(data = fit_b,
-                aes(x = Biome_Broad_Hab , ymin = `Lower CI b`, ymax =  `Upper CI b`, colour = Biome_Broad_Hab),
+  geom_errorbar(data = rich_biome_div,
+                aes(x = Biome_Broad_Hab , ymin = `b_Lower CI`, ymax =  `b_Upper CI`, colour = Biome_Broad_Hab),
                 position = position_dodge(width = 0.75),
                 size = 0.75, width = 0) +
   scale_color_viridis(discrete = T, option="D")  +
@@ -215,14 +280,14 @@ rich_biome_b <- ggplot() +
                                axis.text.x=element_blank(),
                                plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = 0.2, unit = "cm"),
                                plot.title=element_text(size=18, hjust=0.5),
-                               strip.background = element_blank(),legend.position="none") + 
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) + 
-  #coord_cartesian( ylim = c(0,150)) +
-  labs(x='',
-       y = 'Average species richness in the soil seed bank',
-       title = 'beta') 
+                               strip.background = element_blank(), legend.position="bottom") + 
+  #scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) + 
+  # coord_cartesian( ylim = c(0,85)) +
+  ggtitle((expression(paste(italic(beta), '-Diversity (', italic(gamma/alpha), ')', sep = '')))) + 
+  ylab((expression(paste('Average ', italic(beta), '-Diversity ',sep = '')))) +  labs(x=''
+  ) 
 
 
 rich_biome_b
 
-rich_biome_i / rich_biome_b
+(rich_biome_a + rich_biome_g) / (rich_biome_b)
