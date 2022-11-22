903d99523b4aeb42a968889b0adbc7b5b4480de9
Emma Ladouceur <emmala@gmail.com>
2022-11-22 17:32:29 +0100

    seeds

--- code/cluster/seed_m2.R
+++ code/cluster/seed_m2.R
@@ -19,8 +19,8 @@ sb_dat <- sb %>% filter(!is.na(log_Total_Seeds),
           Method = as.factor(Method)) 
 
 
-seeds_m2 <- brm(log_Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + (Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab | Habitat_Degraded ),
-                    family = student(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
+seeds_m2 <- brm(Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + (Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab | Habitat_Degraded ),
+                    family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
                      control = list(adapt_delta = 0.999)
 )
 
