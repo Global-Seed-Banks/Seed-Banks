4e723d765837042502ea36e3605c07926f7e2fea
Emma Ladouceur <emmala@gmail.com>
2022-11-22 17:49:40 +0100

    seeds only

--- code/cluster/seed_m2.R
+++ code/cluster/seed_m2.R
@@ -7,8 +7,9 @@ path <- '/gpfs1/data/idiv_chase/emmala/Seed-Bank-Map'
 sb <- read.csv(paste0(path, '/sb_prep.csv'), header=T, fill=TRUE, sep=",", na.strings=c(""," ","NA","NA ","na"))
 
 # Total_Species   Seed_density_m2   Total_Seeds
-sb_dat <- sb %>% filter(!is.na(log_Total_Seeds),
-                        #!Total_Seeds == 0,
+sb_dat <- sb %>% filter(#!is.na(log_Total_Seeds),
+                        !is.na(Total_Seeds),
+                        !Total_Seeds == 0,
                         !is.na(Centred_log_Total_Sample_Area_m2)) %>%
   # treat all random effects as factors
   mutate( Habitat_Degraded = as.factor(Habitat_Degraded),
@@ -20,7 +21,7 @@ sb_dat <- sb %>% filter(!is.na(log_Total_Seeds),
 
 
 seeds_m2 <- brm(Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + (Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab | Habitat_Degraded ),
-                    family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
+                    family = student(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000,
                      control = list(adapt_delta = 0.999)
 )
 
