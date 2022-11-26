928775586fb696311a13d3fcb624bf1d85c451eb
Emma Ladouceur <emmala@gmail.com>
2022-11-26 12:24:14 +0100

    seed exgaussian

--- code/cluster/seed_m2.R
+++ code/cluster/seed_m2.R
@@ -21,8 +21,8 @@ sb_dat <- sb %>% filter(#!is.na(log_Total_Seeds),
 
 
 
-seeds_m2 <- brm(Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + ( 1  | Habitat_Degraded/Method/studyID ),
-                    family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000, 
+seeds_m2 <- brm(Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + ( 1  | Habitat_Degraded/Method/studyID/rowID ),
+                    family = exgaussian(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000, 
                      control = list(adapt_delta = 0.999)
 )
 
