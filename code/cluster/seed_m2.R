1e3866b48a12a4a07cc288e45cb2e610004d372b
Emma Ladouceur <emmala@gmail.com>
2022-11-27 19:11:07 +0100

    last random effects try

--- code/cluster/seed_m2.R
+++ code/cluster/seed_m2.R
@@ -21,7 +21,7 @@ sb_dat <- sb %>% filter(#!is.na(log_Total_Seeds),
 
 
 
-seeds_m2 <- brm(Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + ( 1  | Method/studyID ),
+seeds_m2 <- brm(Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + ( 1  | Habitat_Degraded/Method/studyID ),
                     family = hurdle_lognormal(), data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000, 
                      control = list(adapt_delta = 0.999, max_treedepth = 13)
 )
