2ebab378de4cfb240631b7a763e1f019aab14cb0
Emma Ladouceur <emmala@gmail.com>
2023-02-02 16:30:35 +0100

    seed m2

--- code/cluster/seed_m2.R
+++ code/cluster/seed_m2.R
@@ -24,10 +24,10 @@ sb_dat <- sb %>% filter(#!is.na(log_Total_Seeds),
 
 seeds_m2 <- brm(Total_Seeds ~ Centred_log_Total_Sample_Area_m2 * Biome_Broad_Hab + ( 1  | Method/studyID ),
                    # family = negbinomial(),
-               # family = Gamma(link="log"),
-                family= lognormal(),
+                family = Gamma(link="log"),
+                #family= lognormal(),
                 data = sb_dat, cores = 4, chains = 4, iter = 4000, warmup = 1000, 
-                     control = list(adapt_delta = 0.99999, max_treedepth = 13)
+                     control = list(adapt_delta = 0.999999, max_treedepth = 13)
 )
 
 
