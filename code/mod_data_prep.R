867ddaf8f7e86c66f4c9bd635f6e180cfe0af520
Emma Ladouceur <emmala@gmail.com>
2023-04-06 13:53:22 +0200

    data summaries for results

--- code/mod_data_prep.R
+++ code/mod_data_prep.R
@@ -65,7 +65,6 @@ head(sb_calc)
 summary(sb_calc)
 
 
-
 # write over biomes when habitat is arable or aquatic
 sb_mod <- sb_calc %>% 
   mutate(Biome_Broad_Hab = case_when(Habitat_Broad %in% c("Arable", "Aquatic") ~ Habitat_Broad ,
@@ -83,41 +82,6 @@ sb_check <- sb_mod %>% filter(Biome_Broad_Hab %in% c( "Aquatic", "Mediterranean 
 View(sb_check)
 
 
-# get a summary of min and max values
-sb_deets <- sb_mod %>% group_by(Biome_Broad_Hab) %>%
-summarise(`min-Total_Number_Samples` = min(as.numeric(Total_Number_Samples), na.rm = TRUE),
-          `max-Total_Number_Samples` = max(as.numeric(Total_Number_Samples),na.rm = TRUE),
-          `min-Number_Sites` = min(as.numeric(Number_Sites), na.rm = TRUE),
-          `max-Number_Sites` = max(as.numeric(Number_Sites),na.rm = TRUE),
-          `min-Sample_Area_mm2` = min(as.numeric(Sample_Area_mm2),na.rm = TRUE),
-          `max-Sample_Area_mm2` = max(as.numeric(Sample_Area_mm2), na.rm = TRUE),
-          # `max-Total_Sample_Area_mm2` = max(as.numeric(Total_Sample_Area_mm2), na.rm = TRUE),
-          # `min-Total_Sample_Area_mm2` = min(as.numeric(Total_Sample_Area_mm2), na.rm = TRUE),
-          `max-Total_Sample_Area_m2` = max(as.numeric(Total_Sample_Area_m2), na.rm = TRUE),
-          `min-Total_Sample_Area_m2` = min(as.numeric(Total_Sample_Area_m2), na.rm = TRUE),
-          # `max-Centred_log_Total_Sample_Area_m2` = max(as.numeric(Centred_log_Total_Sample_Area_m2), na.rm = TRUE),
-          # `min-Centred_log_Total_Sample_Area_m2` = min(as.numeric(Centred_log_Total_Sample_Area_m2), na.rm = TRUE),
-          `min-Sample_Volume_mm3` = min(as.numeric(Sample_Volume_mm3), na.rm = TRUE),
-          `max-Sample_Volume_mm3` = max(as.numeric(Sample_Volume_mm3),na.rm = TRUE),
-          # `min-Total_Sample_Volume_mm3` = min(as.numeric(Total_Sample_Volume_mm3), na.rm = TRUE),
-          # `max-Total_Sample_Volume_mm3`= max(as.numeric(Total_Sample_Volume_mm3),na.rm = TRUE),
-          `min-Total_Sample_Volume_m3` = min(as.numeric(Total_Sample_Volume_m3), na.rm = TRUE),
-          `max-Total_Sample_Volume_m3` = max(as.numeric(Total_Sample_Volume_m3),na.rm = TRUE),
-          `min-Total_Species` = min(as.numeric(Total_Species), na.rm = TRUE),
-          `max-Total_Species` = max(as.numeric(Total_Species),na.rm = TRUE),
-          `min-Seed_density_m2` = min(as.numeric(Seed_density_m2), na.rm = TRUE),
-          `max-Seed_density_m2` = max(as.numeric(Seed_density_m2),na.rm = TRUE),
-          `min-Total_Seeds` = min(as.numeric(Total_Seeds), na.rm = TRUE),
-          `max-Total_Seeds` = max(as.numeric(Total_Seeds),na.rm = TRUE),
-) %>%
-  pivot_longer(`min-Total_Number_Samples` : `max-Total_Seeds`) %>%
-  separate(name, into = c("minmax", "name"), sep="-") %>% spread(minmax, value)
-
-sb_deets
-
-write.csv(sb_deets,  "sb_details_summary.csv")
-write.csv(sb_deets,  "sb_details_biome_summary.csv")
-
 # sb_mod %>% distinct(Biome_Broad_Hab) %>% arrange(Biome_Broad_Hab)
 # 
 # sb_mod %>% select(Biome_Broad_Hab, Total_Sample_Area_m2, Total_Seeds, Total_Species) %>%
@@ -130,126 +94,99 @@ write.csv(sb_mod,  "sb_prep.csv")
 
 
 
-#reload new dat
+#reload new dat and get some summaries
 sb_prep <- read.csv(paste0(path2wd, 'Data/sb_prep.csv'))
 
 
 head(sb_prep)
 
-sb_count <- sb_prep %>% select(Biome_Broad_Hab,  Total_Species) %>%
+biome_count <- sb_prep %>% select(Biome_Broad_Hab,  Total_Species) %>%
   dplyr::group_by(Biome_Broad_Hab) %>%
   count() 
 
-sb_count
+biome_count
+
+sites_count <- sb_prep %>% select(Number_Sites) %>%
+  dplyr::group_by(Number_Sites) %>%
+  count() 
+
+View(sites_count)
+
+options(scipen = 999)
+
+colnames(sb_prep)
+
+# get a summary of min and max values
+# keep country and study details so we can cite them as examples
+
+sb_deets <- sb_prep %>% group_by(Biome_Broad_Hab, Country, studyID) %>%
+  summarise(`min-Total_Number_Samples` = min(as.numeric(Total_Number_Samples), na.rm = TRUE),
+            `max-Total_Number_Samples` = max(as.numeric(Total_Number_Samples),na.rm = TRUE),
+            `min-Number_Sites` = min(as.numeric(Number_Sites), na.rm = TRUE),
+            `max-Number_Sites` = max(as.numeric(Number_Sites),na.rm = TRUE),
+            `min-Sample_Area_mm2` = min(as.numeric(Sample_Area_mm2),na.rm = TRUE),
+            `max-Sample_Area_mm2` = max(as.numeric(Sample_Area_mm2), na.rm = TRUE),
+             `max-ratio_seeds_species` = max(as.numeric(ratio_seeds_species), na.rm = TRUE),
+            `min-ratio_seeds_species` = min(as.numeric(ratio_seeds_species), na.rm = TRUE),
+            `max-Total_Sample_Area_m2` = max(as.numeric(Total_Sample_Area_m2), na.rm = TRUE),
+            `min-Total_Sample_Area_m2` = min(as.numeric(Total_Sample_Area_m2), na.rm = TRUE),
+            `min-Sample_Volume_mm3` = min(as.numeric(Sample_Volume_mm3), na.rm = TRUE),
+            `max-Sample_Volume_mm3` = max(as.numeric(Sample_Volume_mm3),na.rm = TRUE),
+            `min-Total_Sample_Volume_m3` = min(as.numeric(Total_Sample_Volume_m3), na.rm = TRUE),
+            `max-Total_Sample_Volume_m3` = max(as.numeric(Total_Sample_Volume_m3),na.rm = TRUE),
+            `min-Total_Species` = min(as.numeric(Total_Species), na.rm = TRUE),
+            `max-Total_Species` = max(as.numeric(Total_Species),na.rm = TRUE),
+            `min-Seed_density_m2` = min(as.numeric(Seed_density_m2), na.rm = TRUE),
+            `max-Seed_density_m2` = max(as.numeric(Seed_density_m2),na.rm = TRUE),
+            `min-Total_Seeds` = min(as.numeric(Total_Seeds), na.rm = TRUE),
+            `max-Total_Seeds` = max(as.numeric(Total_Seeds),na.rm = TRUE),
+  ) %>%
+  pivot_longer(`min-Total_Number_Samples` : `max-Total_Seeds`) %>%
+  separate(name, into = c("minmax", "name"), sep="-") %>% 
+  #spread(minmax, value) %>%
+  ungroup() %>%
+  filter(#value > 0,
+         !is.infinite(value) ) 
+
+sb_zero <- sb_deets %>%
+  filter(minmax == "min") %>%
+  filter(value == 0)
+
+sb_zero
 
 
+sb_minmax <-  bind_rows( 
+  sb_max <- sb_deets %>%
+  filter(minmax == "max") %>%
+  group_by(name) %>%
+  filter(value == max(value)),
+   sb_min <- sb_deets %>%
+    filter(minmax == "min") %>%
+    filter(value > 0) %>%
+    group_by(name) %>%
+  filter(value == min(value))
+  ) %>%
+  arrange(name, minmax, value ) %>%
+  group_by(name) %>%
+  left_join( bind_rows(
+    sb_max %>% group_by(name, minmax) %>%
+      count() ,
+    sb_min %>% group_by(name, minmax) %>%
+      count() 
+  ) ) 
 
+ min_max <- sb_minmax %>% filter(!n > 1)
+  
+ View(min_max)
 
+studies_mmin_multi <- sb_minmax %>% filter(n > 1)
+ View(studies_mmin_multi)
 
+min_max_count <- sb_minmax %>% filter(n > 1) %>% 
+   select( minmax, value, n) %>%
+   distinct()
+View(min_max_count)
 
-
-
-
-
-
-
-sb_n_hz <- sb_calc %>%  filter(!is.na(Total_Species)) %>%
-          group_by(Biome_WWF_Zone, Habitat_Broad ) %>%
-  summarise(n_dat = n_distinct(rowID))
-
-sb_n_hz
-head(sb_n_hz)
-
-
-ggplot( data = sb_n_hz,
-        aes(x = Habitat_Broad, y = n_dat,
-        color = Habitat_Broad)) + 
-  facet_wrap(~Biome_WWF_Zone, scales= "free") +
-  geom_bar(data = sb_n_hz ,
-             aes(x = Habitat_Broad, y = n_dat,
-                 ),  fill= "white", stat = "identity", position= 'dodge') +
-  scale_color_viridis(discrete = T, option="D")  +
-  theme_classic( ) + labs(y ="Number of data Points", x= "Habitat",
-                          subtitle = "Total Species") +
-  geom_text(aes(label=n_dat), position=position_dodge(width=0.9), vjust=-0.25)
-
-
-
-sb_n_h <- sb_calc %>%  filter(!is.na(Total_Species)) %>%
-  group_by( Habitat_Broad ) %>%
-  summarise(n_dat = n_distinct(rowID))
-
-sb_n_h
-head(sb_n_h)
-
-
-ggplot( data = sb_n_h,
-        aes(x = Habitat_Broad, y = n_dat,
-            color = Habitat_Broad)) + 
-  #facet_wrap(~Biome_WWF_Zone, scales= "free") +
-  geom_bar(data = sb_n_h ,
-           aes(x = Habitat_Broad, y = n_dat,
-           ),  fill= "white", stat = "identity", position= 'dodge') +
-  scale_color_viridis(discrete = T, option="D")  +
-  theme_classic( ) + labs(y ="Number of data Points", x= "Habitat",
-                          subtitle = "Total Species") +
-  geom_text(aes(label=n_dat), position=position_dodge(width=0.9), vjust=-0.25)
-
-
-sb_n_z <- sb_calc %>%  filter(!is.na(Total_Species)) %>%
-  group_by( Biome_WWF_Zone ) %>%
-  summarise(n_dat = n_distinct(rowID))
-
-sb_n_z
-head(sb_n_z)
-
-
-ggplot( data = sb_n_z,
-        aes(x = Biome_WWF_Zone, y = n_dat,
-            color = Biome_WWF_Zone)) + 
-  #facet_wrap(~Biome_WWF_Zone, scales= "free") +
-  geom_bar(data = sb_n_z ,
-           aes(x = Biome_WWF_Zone, y = n_dat,
-           ),  fill= "white", stat = "identity", position= 'dodge') +
-  scale_color_viridis(discrete = T, option="D")  +
-  theme_classic( ) + labs(y ="Number of data Points", x= "Habitat",
-                          subtitle = "Total Species") +
-  geom_text(aes(label=n_dat), position=position_dodge(width=0.9), vjust=-0.25)
-
-
-# first explore data
-# 3 possible metrics for continuous 'sample effort'
-colnames(sb)
-
-ggplot() + 
-  facet_wrap(~Biome_WWF_Zone, scales= "free") +
-  geom_point(data = sb_calc ,#%>% filter(Biome_WWF_Zone == "Boreal"),
-             aes(x = Total_Sample_Volume_mm3, y = Total_Species,
-                 colour = Habitat_Broad),
-             size = 1.2, shape=1, position = position_jitter(width = 2, height=2.5)) +
-  scale_color_viridis(discrete = T, option="D")  +
-  theme_classic( ) 
-
-
-ggplot() + 
-  facet_wrap(~Biome_WWF_Zone, scales = "free") +
-  geom_point(data = sb_calc , #%>% filter(Biome_WWF_Zone == "Boreal"),
-             aes(x = Total_Number_Samples, y = Total_Species,
-                 colour = Habitat_Broad),
-             size = 1.2, shape=1, position = position_jitter(width = 2, height=2.5)) +
-  scale_color_viridis(discrete = T, option="D")  +
-  #xlim(0,1000)
-  coord_cartesian(xlim = c(min(sb_prep_r$Total_Number_Samples), quantile(sb_prep_r$Total_Number_Samples, 0.90)))+
-  theme_classic( ) 
-
-ggplot() + 
-  facet_wrap(~Biome_WWF_Zone, scales = "free") +
-  geom_point(data = sb_calc %>%  group_by(Biome_WWF_Zone) %>% filter(Total_Sample_Area_mm2 > quantile(Total_Sample_Area_mm2, probs=0.025, na.rm = TRUE),
-                                        Total_Sample_Area_mm2 < quantile(Total_Sample_Area_mm2, probs=0.975, na.rm = TRUE)),
-             aes(x = Total_Sample_Area_m2, y = Total_Species,
-                 colour = Habitat_Broad),
-             size = 1.2, shape=1) +
-  scale_color_viridis(discrete = T, option="D")  +
-  #coord_cartesian(xlim = c(min(sb_calc$Total_Sample_Area_m2, na.rm = TRUE), quantile(sb_calc$Total_Sample_Area_m2, 0.97, na.rm = TRUE)))+
-  theme_classic( ) 
+write.csv(sb_deets,  "sb_details_summary.csv")
+write.csv(sb_deets,  "sb_details_biome_summary.csv")
 
