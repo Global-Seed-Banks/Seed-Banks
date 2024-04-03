ba384790627d055509712d50db62bb8c286094e1
Emma Ladouceur <emmala@gmail.com>
2024-04-03 10:36:48 +0200

    new modules

--- code/cluster/rich_seeds.sh
+++ code/cluster/rich_seeds.sh
@@ -8,7 +8,7 @@
 # Output files should ideally go to /work instead of /home
 #SBATCH -o /work/%u/%x-%j.out
 
-module load foss/2019b R/3.6.2-2
+module load foss/2022b R/4.2.2
 export OFILE=/work/$USER/Seed-Banks/$SLURM_JOB_NAME-$SLURM_JOB_ID.Rdata
 export LANG=en_US.UTF-8
 Rscript --vanilla /home/ladouceu/projects/Seed-Banks/code/cluster/rich_seeds.R
