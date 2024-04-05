5e59876632c2d824c1b9cad2e56d62d4bade9a17
Emma Ladouceur <emmala@gmail.com>
2024-04-05 16:07:02 +0200

    aq test

--- code/cluster/den_aq.R
+++ code/cluster/den_aq.R
@@ -23,7 +23,7 @@ sb_dat <- sb %>%
 sb_dat$Habitat_degraded <- relevel(sb_dat$Habitat_degraded, ref = "1")
 
 
-mod_aq_d <- brm(Seed_density_m2 ~  Habitat_degraded + ( 1 | StudyID/RowID ),
+mod_aq_d <- brm(Seed_density_m2 ~  Habitat_degraded + ( 1 | StudyID ),
                   family= lognormal(),
                   data = sb_dat, cores = 4, chains = 4, iter = 10000, warmup = 1000, 
                    control = list(adapt_delta = 0.9999)
