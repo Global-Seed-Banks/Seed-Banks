3fe2070072c4c2f69a6fea4e6ba41af0730b785b
Emma Ladouceur <emmala@gmail.com>
2024-04-06 17:34:07 +0200

    time

--- code/cluster/den_wetland.sh
+++ code/cluster/den_wetland.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -J den_wetland
-#SBATCH --time=60:00:00
+#SBATCH --time=5:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=4
 
--- code/cluster/ratio_po_alp.sh
+++ code/cluster/ratio_po_alp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -J ratio_po_alp
-#SBATCH --time=60:00:00
+#SBATCH --time=5:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=4
 
