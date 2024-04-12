5fe38a03e12dd95b4483d01cd8033fa336a248ef
Emma Ladouceur <emmala@gmail.com>
2024-04-12 15:02:22 +0200

    habs

--- code/cluster/ratio_ar.R
+++ code/cluster/ratio_ar.R
@@ -17,11 +17,7 @@ sb_dat <- sb %>%
           StudyID = as.factor(StudyID),
           RowID = as.factor(RowID),
           Method = as.factor(Method)) %>% arrange(Biome_broad_hab) %>%
-  filter(Realm == "Arable") %>%
-  mutate(Biome = case_when(grepl("Deserts", Biome) ~ "Mediterranean and Desert",
-                           grepl("Temperate", Biome) ~ "Temperate and Boreal",
-                           grepl("Boreal", Biome) ~ "Temperate and Boreal",
-                           grepl("Mediterranean", Biome) ~ "Mediterranean and Desert", TRUE ~ Biome))
+  filter(Realm == "Arable")
 
 
 mod_ar_ra <- brm(ratio_seeds_species ~  Biome + (1 | StudyID ),
