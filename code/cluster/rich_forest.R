5e283247abea531ec3b5a2c6ea9bad464eed0fa5
Emma Ladouceur <emmala@gmail.com>
2024-04-12 17:03:35 +0200

    foressst

--- code/cluster/rich_forest.R
+++ code/cluster/rich_forest.R
@@ -26,12 +26,11 @@ sb_dat$Habitat_degraded <- relevel(sb_dat$Habitat_degraded, ref = "1")
 
 
 mod_forest_r <- brm(Total_species ~ Centred_log_total_sample_area_m2 * Biome  * Habitat_degraded + Centred_log_number_sites + ( 1  | StudyID/RowID ),
-                  family = poisson(), data = sb_dat, cores = 4, chains = 4, iter = 3000, warmup = 1000,
+                  family = poisson(), data = sb_dat, cores = 4, chains = 4, #iter = 3000, warmup = 1000,
                   prior = c(prior( student_t(3, 0.5, 1) , class = b,  lb = 0)),
-                  control = list(adapt_delta = 0.9 #, max_treedepth = 14
+                  control = list(adapt_delta = 0.91 #, max_treedepth = 14
                                  )
 )
-
 
 
 
